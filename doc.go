323dba3e7485c275736ec173c839e928e5f15c44
Nik <njunger@uwaterloo.ca>
2015-02-05 13:14:18 -0500

    Reformat copyright blocks

--- doc.go
+++ doc.go
@@ -1,25 +1,23 @@
-/*
-	Copyright © 2015 Nik Unger
-
-	This file is part of The PBC Go Wrapper.
-
-	The PBC Go Wrapper is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Lesser General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	The PBC Go Wrapper is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public License
-	along with The PBC Go Wrapper. If not, see <http://www.gnu.org/licenses/>.
-
-	The PBC Go Wrapper makes use of The PBC library. The PBC Library and its
-	use are covered under the terms of the GNU Lesser General Public License
-	version 3, or (at your option) any later version.
-*/
+// Copyright © 2015 Nik Unger
+//
+// This file is part of The PBC Go Wrapper.
+//
+// The PBC Go Wrapper is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// The PBC Go Wrapper is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with The PBC Go Wrapper. If not, see <http://www.gnu.org/licenses/>.
+//
+// The PBC Go Wrapper makes use of The PBC library. The PBC Library and its use
+// are covered under the terms of the GNU Lesser General Public License
+// version 3, or (at your option) any later version.
 
 /*
 	Package pbc provides structures for building pairing-based cryptosystems. It
